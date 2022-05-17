d91a4846d9f7f8bf0b6d67f9d5b6c6e70e9fed92
EurisG <euris809@gmail.com>
2022-05-17 11:00:09 -0400

    react

--- Labs/newFruitsApp/views/Show.jsx
+++ Labs/newFruitsApp/views/Show.jsx
@@ -2,8 +2,18 @@ const React = require('react')
 
 class Show extends React.Component {
     render () {
+
+        const fruit = this.props.fruit
+
+
         return (
+            <>
             <h1> Show Page </h1>
+
+            The {fruit.name} is {fruit.color}
+            {" "}
+            {fruit.readyToEat ? '. It is Ready to Eat!' : ". Its no Good!"}
+            </>
         );
     }
 }
