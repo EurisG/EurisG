67e80cd5bcd1c3c77768ca458b057023404b61b8
EurisG <euris809@gmail.com>
2022-05-18 14:43:19 -0400

    exported the learnerController

--- Labs/firstApi/app.js
+++ Labs/firstApi/app.js
@@ -2,54 +2,57 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// Refactor route controllers
-// This is to retrieve all the learners at once
-const getAllLearners = (request, response) => {
-    response.status(500).json({
-      status: "fail",
-      data: {
-        message: "undefined routes",
-      },
-    });
-  };
+const learnerController = require('./controllers/learnerController')
 
-//  This controller is to create a new learner
-const createLearner = (request, response) => {
-  response.status(500).json({
-    status: "fail",
-    data: {
-      message: "undefined routes",
-    },
-  });
-};
-// This controller is to retrieve a single learner
-const getSingleLearner = (request, response) => {
-    response.status(500).json({
-      status: "fail",
-      data: {
-        message: "undefined routes",
-      },
-    });
-  };
-  // This controller is to update a single learner data
-const updateLearner = (request, response) => {
-    response.status(500).json({
-      status: "fail",
-      data: {
-        message: "undefined routes",
-      },
-    });
-  };
+console.log(learnerController)
+// // Refactor route controllers
+// // This is to retrieve all the learners at once
+// const getAllLearners = (request, response) => {
+//     response.status(500).json({
+//       status: "fail",
+//       data: {
+//         message: "undefined routes",
+//       },
+//     });
+//   };
 
-// This controller is to delete a single learner
-const deleteLearner = (request, response) => {
-  response.status(500).json({
-    status: "fail",
-    data: {
-      message: "undefined routes",
-    },
-  });
-};
+// //  This controller is to create a new learner
+// const createLearner = (request, response) => {
+//   response.status(500).json({
+//     status: "fail",
+//     data: {
+//       message: "undefined routes",
+//     },
+//   });
+// };
+// // This controller is to retrieve a single learner
+// const getSingleLearner = (request, response) => {
+//     response.status(500).json({
+//       status: "fail",
+//       data: {
+//         message: "undefined routes",
+//       },
+//     });
+//   };
+//   // This controller is to update a single learner data
+// const updateLearner = (request, response) => {
+//     response.status(500).json({
+//       status: "fail",
+//       data: {
+//         message: "undefined routes",
+//       },
+//     });
+//   };
+
+// // This controller is to delete a single learner
+// const deleteLearner = (request, response) => {
+//   response.status(500).json({
+//     status: "fail",
+//     data: {
+//       message: "undefined routes",
+//     },
+//   });
+// };
 
 // create an express router 
 const learnerRouter = express.Router();
@@ -58,8 +61,8 @@ const learnerRouter = express.Router();
 app.use('/api/v1/learners', learnerRouter);
 
 // lets refactor the route 
-learnerRouter.route("/").get(getAllLearners).post(createLearner);
-learnerRouter.route("/:id").get(getSingleLearner).patch(updateLearner).delete(deleteLearner);
+learnerRouter.route("/").get(learnerController.getAllLearners).post(learnerController.createLearner);
+learnerRouter.route("/:id").get(learnerController.getSingleLearner).patch(learnerController.updateLearner).delete(learnerController.deleteLearner);
 
 
 
