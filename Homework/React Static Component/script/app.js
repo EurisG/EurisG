0a3dca59737c5f7e99c737e9bca400e4564868af
EurisG <euris809@gmail.com>
2022-06-04 16:38:15 -0400

    components are styled and in parent container but needs work

--- Homework/React Static Component/script/app.js
+++ Homework/React Static Component/script/app.js
@@ -1,58 +1,75 @@
 // styling 
 const body = {
- border: "solid 2px red",
- margin: "20px",
- height:"700px",
- backgroundColor: "grey"
+ margin: "200px",
+ backgroundColor: "#b4b4b4",
 }
 
 const bodyOne = {
-    padding: "30px"
+    padding: "30px",
+    // display: "flex",
+    // justifyContent: "flexEnd"
+}
+
+const bodyThree = {
+    padding: "30px",
+    
+    
 }
 
 const boxOne = {
-    border: "solid 2px black",
-    padding: "40px",
+    // border: "solid 2px black",
+    padding: "50px",
     width: "70px",
-    height: "550px",
+    height: "650px",
     backgroundColor: "lightGrey",
-    textAlign: "center"
+    textAlign: "center",
+    
+}
+const boxOneText = {
+    top: "20%",
+    position: "relative"
 }
 
 const boxTwo = {
-    border : "solid 2px black",
-    padding: "40px",
-    width: "50px",
-    height: "250%",
+    // border : "solid 2px black",
+    // padding: "40px",
+    width: "170px",
+    height: "250px",
     backgroundColor: "lightGrey",
-   
-    // display: "flex",
-    justifyContent: "spaceAround"
+    margin: "10px",
+    position: "relative",
+    top: 150,
 }
 
 const boxDivTwo = {
    
     padding: "40px",
-    width: "50px",
+    // width: "150px",
     height: "60px",
+    alignitems: "center",
     display: "flex",
-    justifyContent: "space-between",
-    // position: "relative",
-    // top: "0"
+    justifyItems: "right",
+    marginLeft: "150px"
+   
 }
 
 const boxThree = {
-    border : "solid 2px black",
+    // border : "solid 2px black",
     padding: "40px",
-    width: "550px",
-    height: "250px",
-    backgroundColor: "lightGrey"
+    // width: "550px",
+    height: "350px",
+    backgroundColor: "lightGrey",
+    margin: "10px",
+    marginLeft: "27%",
+    flexDirection: "flex",
+    position: "relative",
+    bottom: 550,
 }
 
 const boxInner = {
-    border : "solid 2px black",
-    padding: "40px",
-    width: "450px",
+    // border : "solid 2px black",
+    padding: "70px",
+    // width: "450px",
     height: "100px",
     backgroundColor: "grey"
 }
@@ -60,15 +77,15 @@ const boxInner = {
 class LeftBox extends React.Component {
     render() {
         return(
-          <div style={body}>
+          <div>
               <div style={bodyOne}>
                 <div style={boxOne}>
-                <h4>Dashboard</h4>
-                <h4>Widget</h4>
-                <h4>Reviews</h4>
-                <h4>Customers</h4>
-                <h4>Online Analyst</h4>
-                <h4>Settings</h4>
+                <h4 style={boxOneText}>Dashboard</h4>
+                <h4 style={boxOneText}>Widget</h4>
+                <h4 style={boxOneText}>Reviews</h4>
+                <h4 style={boxOneText}>Customers</h4>
+                <h4 style={boxOneText}>Online Analyst</h4>
+                <h4 style={boxOneText}>Settings</h4>
                 </div>
                 </div>
            </div>
@@ -80,26 +97,27 @@ class TopBox extends React.Component {
     render() {
         return(
             
+        <div style={bodyThree}>
             <div style={boxDivTwo}>
-            <div style={boxTwo}>
-                <p>Reviews</p>
-                <h3>1,281</h3>
-            </div>
+                <div style={boxTwo}>
+                    <h5>Reviews</h5>
+                    <h3>1,281</h3>
+                 </div>
 
             <div style={boxTwo}>
-                <p>Average Rating</p>
+                <h5>Average Rating</h5>
                 <h3>4.6</h3>
             </div>
 
             <div style={boxTwo}>
-                <p>Sentiment Analyst</p>
+                <h5>Sentiment Analyst</h5>
                 <h3>960</h3>
                 <h3>122</h3>
                 <h3>321</h3>
             </div>
 
-        </div>
-       
+             </div>
+       </div>
         );
     }
 };
@@ -107,12 +125,12 @@ class TopBox extends React.Component {
 class BottomBox extends React.Component {
     render() {
         return(
-            <div style={boxThree}>
-                <p>Website Visitors</p>
-                <h3>821</h3>
-                <div style={boxInner}>
-
-                </div>
+            <div style={bodyOne}>
+                <div style={boxThree}>
+                    <h5>Website Visitors</h5>
+                    <h3>821</h3>
+                     <div style={boxInner}></div>
+                 </div>
             </div>
         )
     }
@@ -121,15 +139,13 @@ class BottomBox extends React.Component {
 class App extends React.Component {
     render() {
         return(
-            <div>
-             <div>
-                 <LeftBox />
-                    <TopBox />
-                    
-                 <BottomBox />
-            </div>
-               
-             
+            <div style={body}>
+                 <TopBox />
+                <LeftBox /> 
+                <BottomBox />
+                
+                  
+                 {/* <BottomBox /> */}
            
             </div>
         );
