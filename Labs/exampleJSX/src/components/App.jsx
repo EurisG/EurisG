c90afe6f35ab6dfb99121f4afc04eb50d67631bb
EurisG <euris809@gmail.com>
2022-06-10 11:47:51 -0400

    practice

---
+++ Labs/exampleJSX/src/components/App.jsx
@@ -0,0 +1,13 @@
+// IMPORT REACT 
+import React from 'react';
+
+// RETURN APP COMPONENT 
+const App = () => {
+    return(
+        <div>
+            <h1>App Component</h1>
+        </div>
+    );
+};
+// EXPORT APP COMPONENT
+export default App;
---
+++ Labs/exampleJSX/src/index.jsx
@@ -0,0 +1,9 @@
+// IMPORT REACT
+import React from "react";
+// IMPORT REACT-DOM
+import ReactDOM from 'react-dom';
+// IMPORT APP
+import App from "./components/App";
+
+// RENDER YOUR APP 
+ReactDOM.render(<App />, document.querySelector('#root'))
