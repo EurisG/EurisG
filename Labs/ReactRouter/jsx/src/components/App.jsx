670f0ba41ab2819a67c204fb867677939933af38
EurisG <euris809@gmail.com>
2022-06-22 11:58:00 -0400

    routes

--- Labs/ReactRouter/jsx/src/components/App.jsx
+++ Labs/ReactRouter/jsx/src/components/App.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
 
+import { Route, Switch } from 'react-router-dom';
 import Navigation from './Navigation';
+import Home from './Home';
+import About from './About';
+import Projects from './Projects';
 
 const App = () => {
     return(
         <div className='ui container' style={{marginTop: '30px'}}>
             <Navigation />
+
+            {/* Access to Router Props NOT custom props */}
+            {/* <Route path='/home' component={Home} />
+            <Route path='/about' component={About} />
+            <Route path='/projects' component={Projects} /> */}
+                <Switch>
+                <Route 
+                exact
+                path='/' render={(routerProps) => {
+                    return <Home {...routerProps} homeData='This is data from home component'/>
+                }}/>
+
+                  <Route path='/about' render={() => {
+                    return <About />
+                }}/>
+
+                <Route path='/projects' render={() => {
+                    return <Projects />
+                }}/>
+                </Switch>
         </div>
     )
 };
--- Labs/ReactRouter/jsx/src/components/Navigation.jsx
+++ Labs/ReactRouter/jsx/src/components/Navigation.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const Navigation = () => {
   return (
-    <div class="ui secondary pointing menu">
-      <a class="active item">Home</a>
-      <a class="item">Messages</a>
-      <a class="item">Friends</a>
-      <div class="right menu">
-        <a class="ui item">Logout</a>
+    <div className="ui secondary pointing menu">
+      <a className="active item">Home</a>
+      <a className="item">About</a>
+      <a className="item">Projects</a>
+      <div className="right menu">
+        <a className="ui item">Logout</a>
       </div>
     </div>
   );
