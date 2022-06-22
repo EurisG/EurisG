a56dbae6058b6e04cc60ebc52031d8309a11dd05
EurisG <euris809@gmail.com>
2022-06-22 11:26:59 -0400

    adding navbar

---
+++ Labs/ReactRouter/jsx/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Navigation from './Navigation';
+
+const App = () => {
+    return(
+        <div className='ui container' style={{marginTop: '30px'}}>
+            <Navigation />
+        </div>
+    )
+};
+
+export default App;
