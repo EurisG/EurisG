ced9e6872536064c4ffd6111c4b42927ee9d2fbf
EurisG <euris809@gmail.com>
2022-06-22 12:01:11 -0400

    components are linked to app component

--- Labs/ReactRouter/jsx/src/components/Navigation.jsx
+++ Labs/ReactRouter/jsx/src/components/Navigation.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const Navigation = () => {
   return (
     <div className="ui secondary pointing menu">
-      <a className="active item">Home</a>
-      <a className="item">About</a>
-      <a className="item">Projects</a>
+      <a className="active item" href="/">Home</a>
+      <a className="item" href="/about">About</a>
+      <a className="item" href="/projects">Projects</a>
       <div className="right menu">
         <a className="ui item">Logout</a>
       </div>
