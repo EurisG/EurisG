8a69c2539052b5a20bc829570e90d46b9b810319
EurisG <euris809@gmail.com>
2022-06-23 12:22:05 -0400

    user admin

--- Labs/ReactApi/jsx/src/components/App.jsx
+++ Labs/ReactApi/jsx/src/components/App.jsx
@@ -3,8 +3,8 @@ import User from './User';
 
 const App = () => {
     return(
-        <div>
-            <h1>App Component</h1>
+        <div className='ui container' style={{marginTop: '20px'}}>
+            {/* <h1>App Component</h1> */}
             <User />
         </div>
     )
