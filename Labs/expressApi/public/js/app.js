7c3df0c817dd2a23af6fa0a16330aa9a809f857b
EurisG <euris809@gmail.com>
2022-06-30 14:10:49 -0400

    console logging the new product

--- Labs/expressApi/public/js/app.js
+++ Labs/expressApi/public/js/app.js
@@ -1,58 +1,102 @@
-// CREATE APP COMPONENT
+  // CREATE APP COMPONENT
 class App extends React.Component {
-    state = {
-      todos: [],
-    };
-    // Add your lifecycle method here
-    componentDidMount() {
-      fetch("/todos")
-        .then((response) => {
-          return response.json();
-        })
-        .then((todos) => {
-          this.setState({ todos: todos.data.todos });
-        })
-        .catch((error) => {
-          console.error(error.message);
-        });
-    }
-
-    // CREATE A DELETE TODO METHOD to delete todos
-    deleteToDo = (id, index) => {
-      // console.log(id, index)
-      fetch(`todos/${id}`, {method: 'DELETE'})
-      .then(() => { this.setState({
-        todos: [
-          // SLICE TO DETERMINE WHICH INDEX WAS CHOSEN
-          ...this.state.todos.slice(0, index),
-          // SLICE INDEX THAT WAS CHOSEN AND DISPLAYING NEW ARRAY
-          ...this.state.todos.slice(index + 1),
-        ]
+  state = {
+    todos: [],
+    description: "",
+  };
+  // Add your lifecycle method here
+  componentDidMount() {
+    fetch("/todos")
+      .then((response) => {
+        return response.json();
       })
-       
+      .then((todos) => {
+        this.setState({ todos: todos.data.todos });
       })
-    }
-    render() {
-      return (
-        <ul>
-         
-            {this.state.todos.map((todo, index) => {
-              return (
-                <li>
-                   {todo.description} 
-                   {" "}
-                  <button onClick={() => {
-                    return this.deleteToDo(todo._id, index);
-                  }}> Completed </button>
-                  <br />
-                  <small> (Need to complete) </small>
-                </li>
-              );
-            })}
-          
-        </ul>
-      );
-    }
+      .catch((error) => {
+        console.error(error.message);
+      });
   }
-  // RENDER COMPONENT ON THE SCREEN
-  ReactDOM.render(<App />, document.querySelector(".container"));
+  // Create a on change method to keep track of whatever the user is typing inside our input box
+  handleChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value,
+    });
+  };
+  // Lets create a method to help submit our form
+  handleFormSubmission = (e) => {
+       // STOPS RELOADING THE PAGE AFTER SUBMITTING
+    e.preventDefault();
+    fetch("/todos", {
+        // changing description to a string in order to post
+      body: JSON.stringify({ description: this.state.description }),
+      method: "POST",
+      headers: {
+          // CAN FIND HEADERS IN POSTMAN
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error(error.message);
+      });
+  };
+  // Create a delete todo method
+  deleteToDo = (id, index) => {
+     // console.log(id, index)
+    fetch(`todos/${id}`, { method: "DELETE" }).then(() => {
+      this.setState({
+        todos: [
+          ...this.state.todos.slice(0, index), // Take everything before what the user clicked on!
+          ...this.state.todos.slice(index + 1), // Take everything after what the user clicked on!
+        ],
+      });
+    });
+  };
+  render() {
+    return (
+      <div>
+        <h1> To Dos </h1>
+        <form onSubmit={this.handleFormSubmission}>
+          <label htmlFor="description">Description</label>
+          <input
+            type="text"
+            value=""
+            id="description"
+            onChange={this.handleChange}
+            value={this.state.description}
+          />
+          <input type="submit" />
+        </form>
+        <h2>{this.state.description}</h2>
+        <hr />
+        <ul>
+          {this.state.todos.map((todo, index) => {
+            return (
+              <li>
+                {todo.description}{" "}
+                <button
+                  onClick={() => {
+                    return this.deleteToDo(todo._id, index);
+                  }}
+                >
+                  {" "}
+                  X{" "}
+                </button>
+                <small> complete </small>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
+// RENDER COMPONENT ON THE SCREEN
+ReactDOM.render(<App />, document.querySelector(".container"));
